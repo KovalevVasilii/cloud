adaf204e05b2b5cebc8bb640be44bba3b4ddb725
Kovalev Vasilii <minskij10@gmail.com>
2019-04-27 20:24:30 +0300

    Fixes

--- Cloud/Assets/Scripts/Parameters/Parameter.cs
+++ Cloud/Assets/Scripts/Parameters/Parameter.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Parameter<T> : ITitle
 {
     [SerializeField]
-    private T value;
+    private T value { set; get; }
     
     [SerializeField]
     private string title;
@@ -12,11 +12,6 @@ public class Parameter<T> : ITitle
     public string GetTitle()
     {
         return title;
-    }
-
-    public T GetValue()
-    {
-        return value;
     }
 
     public Parameter(string title, T value)
